4bbd309e98729373cea1af5d0a660d20c921132d
Stefan Kogelnik <stefan.kogelnik@edu.fh-joanneum.at>
2019-10-31 11:12:41 +0100

    initial commmit

---
+++ Backend/src/test/java/at/fhjoanneum/weBuy/WeBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhjoanneum.weBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

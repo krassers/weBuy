d6d1f61650f32c063567da5ba505e6f27a1af481
goeschlp15 <philipp.goeschl@edu.fh-joanneum.at>
2019-11-15 14:49:26 +0100

    generated components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    CheckoutComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,

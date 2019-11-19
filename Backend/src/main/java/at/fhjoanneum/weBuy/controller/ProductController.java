535e9684a38da89edb2b9a51d50da6f21671ff0e
goeschlp15 <philipp.goeschl@edu.fh-joanneum.at>
2019-11-19 23:16:22 +0100

    cleaned imports of ProductController

--- Backend/src/main/java/at/fhjoanneum/weBuy/controller/ProductController.java
+++ Backend/src/main/java/at/fhjoanneum/weBuy/controller/ProductController.java
@@ -4,8 +4,6 @@ import at.fhjoanneum.weBuy.model.Product;
 import at.fhjoanneum.weBuy.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;

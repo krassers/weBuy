35187d49afe6ad65d85ad9548933831b9c416c5d
krassers <stefan.krasser@edu.fh-joanneum.at>
2019-12-18 12:42:30 +0100

    fixed some things that prevented jar file from being executed

--- Backend/src/main/java/at/fhjoanneum/weBuy/repository/UserRepository.java
+++ Backend/src/main/java/at/fhjoanneum/weBuy/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package at.fhjoanneum.weBuy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import at.fhjoanneum.weBuy.model.User;
 
+@RepositoryRestResource()
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }

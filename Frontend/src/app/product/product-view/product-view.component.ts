a34b4b33b6bb3998a285bfe25b5c4cdff8c09331
goeschlp15 <philipp.goeschl@edu.fh-joanneum.at>
2019-12-18 12:52:18 +0100

    added payment service redirect

--- Frontend/src/app/product/product-view/product-view.component.ts
+++ Frontend/src/app/product/product-view/product-view.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/api/product';
 import { Status } from 'src/app/api/types';
 import { UserService } from 'src/app/services/user.service';
+import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-product-view',
@@ -18,7 +20,9 @@ export class ProductViewComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private http: HttpClient,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -43,7 +47,31 @@ export class ProductViewComponent implements OnInit {
     this.productService.update(product).subscribe(
       res => {console.log('### UPDATE:', res)}
     )   
-    
+  }
+
+  public createOrderAndPay(product: Product){
+    console.log('### Create Order ###');
+
+    const apiGatewayId = "1e57f2ac18c3a4fdc55feaa3fe0845824ea13f3a41ef09443370033754ae476a";
+
+    // const params = new URLSearchParams();
+    // params.set('amount', product.sellingPrice.toString());
+    // params.set('callback_data', 'your custom data');
+    // console.log('### Http Post Params: ', params[1], params['callback_data']);
+
+    // this.http.post(`https://gateway.gear.mycelium.com/gateways/${apiGatewayId}/orders?amount=${product.sellingPrice.toString()}&callback_data="yourCustomData"`, {})
+    // .pipe(
+    //   map((response) => {return response})
+    // )
+
+    this.http.post(`https://gateway.gear.mycelium.com/gateways/1e57f2ac18c3a4fdc55feaa3fe0845824ea13f3a41ef09443370033754ae476a/orders?amount=150&callback_data=yourCustomData`, {})
+    .subscribe(
+      (response) => {
+        console.log('###', response);
+        window.location.href =  `https://gateway.gear.mycelium.com/pay/${response['payment_id']}`
+      }
+    )
+    // this.http.post(`https://gateway.gear.mycelium.com/gateways/:apiGatwayId/orders?amount=150&callback_data=yourCustomData`)
   }
 
 }
